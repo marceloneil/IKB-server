1e027a5723519b1bfd933010a589b587eb7fe9af
Marcel O'Neil <marcel@marceloneil.com>
2017-08-30 23:00:09 -0400

    Testing

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ Promise.promisifyAll(web3)
 const kleinABI = require('./IKB.json')
 const kleinContract = new web3.eth.Contract(kleinABI)
 // console.log(kleinContract)
-console.log(kleinContract.options)
+// console.log(kleinContract.options)
 kleinContract.options.address = '0x88ae96845e157558ef59e9ff90e766e22e480390'
-console.log(kleinContract.options)
-const kleinInstance = kleinContract.at('0x88ae96845e157558ef59e9ff90e766e22e480390')
+// console.log(kleinContract.options)
+// const kleinInstance = kleinContract.at('0x88ae96845e157558ef59e9ff90e766e22e480390')
 
 app.get('/', function (req, res) {
   let contract
-  kleinInstance.currentSeries.call().then(currentSeries => {
+  kleinContract.currentSeries.call().then(currentSeries => {
     contract.currentSeries = currentSeries.toNumber()
-    return kleinInstance.issuedToDate.call()
+    return kleinContract.issuedToDate.call()
   }).then(issuedToDate => {
     contract.issuedToDate = issuedToDate.toNumber()
-    return kleinInstance.series.call(contract.currentSeries)
+    return kleinContract.series.call(contract.currentSeries)
   }).then(series => {
     contract.price = series[0].toNumber()
-    return kleinInstance.burnedToDate.call()
+    return kleinContract.burnedToDate.call()
   }).then(burnedToDate => {
     contract.burnedToDate = burnedToDate.toNumber()
-    return kleinInstance.maxSupplyPossible.call()
+    return kleinContract.maxSupplyPossible.call()
   }).then(maxSupplyPossible => {
     contract.maxSupplyPossible = maxSupplyPossible.toNumber()
-    return kleinInstance.balanceOf.call(kleinInstance.address)
+    return kleinContract.balanceOf.call(kleinContract.address)
   }).then(contractBalance => {
     contract.contractBalance = contractBalance.toNumber()
     res.send(contract)
