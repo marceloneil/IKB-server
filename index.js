74a623d06340e5a9cee5c5599b5b825063c9bd10
Marcel O'Neil <marcel@marceloneil.com>
2017-08-30 20:35:51 -0400

    Testing

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express')
 const Web3 = require('web3')
 
+const app = express()
 const web3 = new Web3('ws://parity:8545')
-console.log(web3)
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
