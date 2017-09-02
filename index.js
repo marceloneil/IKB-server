866cf536e257d4a7295e305661a6ab1302b6a661
Marcel O'Neil <marcel@marceloneil.com>
2017-09-02 16:04:16 -0400

    testing

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const web3 = new Web3()
 web3.setProvider(new web3.providers.HttpProvider('ws://parity:8545'))
 
 const kleinABI = require('./IKB.json')
-const kleinContract = new web3.eth.Contract(kleinABI)
-console.log(kleinContract)
-const test = kleinContract.at('0x88ae96845e157558ef59e9ff90e766e22e480390')
-kleinContract.options.address = '0x88ae96845e157558ef59e9ff90e766e22e480390'
+const kleinContract = web3.eth.contract(kleinABI)
+// const kleinContract = new web3.eth.Contract(kleinABI)
+// console.log(kleinContract)
+// const test = kleinContract.at('0x88ae96845e157558ef59e9ff90e766e22e480390')
+// kleinContract.options.address = '0x88ae96845e157558ef59e9ff90e766e22e480390'
 
 app.get('/', function (req, res) {
   let contract
