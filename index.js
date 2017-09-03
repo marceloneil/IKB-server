9596ed9ac9e93b2c6c6c40bfe0ff5cf2b1a7f6cc
Marcel O'Neil <marcel@marceloneil.com>
2017-09-02 20:58:15 -0400

    Final

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get('/', function (req, res) {
   })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, () => {
+  console.log('IKB server listening on port 3000!')
 })
